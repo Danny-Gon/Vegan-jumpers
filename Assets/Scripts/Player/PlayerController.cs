7a3bb78deea70870012abb37edee8bb342e110c3
Danny-Gon <correodedanny@gmail.com>
2024-06-05 17:01:45 -0500

    Escena 2
    Se crea una nueva escena con la mecanica de pista con vista lateral

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    //private float speed = 30; -- Sobra, hace parte del movimiendo
+
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        
+    }
+
+   
+    void Update()
+    {
+        //transform.Translate(Vector3.right * Time.deltaTime * speed); -- Sobra es la del mov de frente
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
---
+++ Assets/Scripts/Spawn/SpawnManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab;
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        //Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); Esto también mirar si es útil
+    }
+
+    void SpawnObstacle()
+    {
+        Vector3 spawnPos = new Vector3(25, 0, 0);
+        int index = Random.Range(0, obstaclePrefab.Length);
+
+
+        //Esto se puede dejar por si vamos a finalizar el juego. Por ahora, en espera de código de tiempo
+        /*if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab[index], spawnPos, obstaclePrefab[index].transform.rotation);
+        }*/
+    }
+}
