c32cf51770cd34f141d4c34d45f848172b2c51bc
Laura Domínguez <131621014+lauradom24@users.noreply.github.com>
2024-06-05 17:47:08 -0500

    Actualización move left script
    Agregue el código de move left

--- Assets/Scripts/Background/MoveLeft.cs
+++ Assets/Scripts/Background/MoveLeft.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 25;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
+
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        /*if (playerControllerScript.gameOver == false) --- Esto si vamos a finalizar el juego cuando chcoca con un objeto
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }*/
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
