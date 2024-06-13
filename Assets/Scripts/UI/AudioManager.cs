3fe41f1cd50e25b4bcc5f4d43f42de582e5b369a
Laura Domínguez <131621014+lauradom24@users.noreply.github.com>
2024-06-12 22:20:34 -0500

    Menú de opciones
    Se agrega el menu de opciones y se ajusta el volumen

--- Assets/Scripts/UI/AudioManager.cs
+++ Assets/Scripts/UI/AudioManager.cs
@@ -6,21 +6,26 @@ public class AudioManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public Slider volumeSlider;
+    public Image imagenMute;
 
-    void Start()
+    private void Start()
     {
-        // Pone el valor del volumen al que está inicialmente
-        float volume;
-        audioMixer.GetFloat("volume", out volume);
-        volumeSlider.value = Mathf.Pow(10, volume / 20);
-
-        // Add listener for slider value change
-        volumeSlider.onValueChanged.AddListener(SetVolume);
+        SetMusicVolume();
     }
-
-    public void SetVolume(float volume)
+    public void SetMusicVolume()
     {
-        // Convert slider value to logarithmic scale
-        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        float volume = volumeSlider.value;
+        audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+
+        /*if (volumeSlider.value == 0)
+        {
+            imagenMute.SetActive(true);
+        }
+        else
+        {
+            imagenMute.SetActive(false);
+        }*/
+        
+
     }
 }
